1ac998e9425df8fe3e089084b0ae199e1f43f641
Isha Kashif <ishak@udel.edu>
2025-08-14 13:10:53 -0400

    guessing dictionary made

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Hangman in Python
 import random
 words = ("apple", "orange", "banana", "coconut", "pineapple")
+
+#dictionary of key: ()
+hangman_art = {0:(),
+               1:(),
+               2:(),
+               3:(),
+               4:(),
+               5:(),
+               6:()}
